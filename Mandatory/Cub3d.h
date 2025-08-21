bd4e0d2016dd5ac0610056ecfce3d7dac9403568
ouel-afi <elafiaouail44@gmail.com>
2025-08-21 13:24:20 +0100

    parsing

--- Mandatory/Cub3d.h
+++ Mandatory/Cub3d.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
 #include "../MLX42/include/MLX42/MLX42.h"
-
 
 #endif
--- Mandatory/main.c
+++ Mandatory/main.c
@@ -1,6 +1,15 @@
 #include "Cub3d.h"
 
-int main()
+int main(int ac, char **av)
 {
-
+	if (ac != 2 || strcmp(av[1] + strlen(av[1]) - 4, ".cub") != 0)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	if (open(av[1], O_RDONLY) == -1)
+	{
+		printf("Error1\n");
+		return 1;
+	}
 }
