08c315f777853d4a9b341f13742d4f0a0a411e3e
Abde Nnour Chat <achat@c1r7p8.1337.ma>
2025-08-26 17:44:54 +0100

    now we stor the value

--- Mandatory/main.c
+++ Mandatory/main.c
@@ -1,5 +1,33 @@
 #include "Cub3d.h"
 
+void add_data(t_data *data, char *identifier, char *value)
+{
+    if (!strcmp(identifier, "NO"))
+        data->map->no_path = ft_strdup(value);
+    else if (!strcmp(identifier, "SO"))
+        data->map->so_path = ft_strdup(value);
+    else if (!strcmp(identifier, "EA"))
+        data->map->ea_path = ft_strdup(value);
+    else if (!strcmp(identifier, "WE"))
+        data->map->we_path = ft_strdup(value);
+}
+
+void add_color(t_data *data, char *values, int r, int g, int b)
+{
+    if (!strcmp(values, "F"))
+	{
+        data->map->f_rgb[0] = ft_atoi(values[1]);
+        data->map->f_rgb[1] = ft_atoi(values[2]);
+        data->map->f_rgb[2] = ft_atoi(values[3]);
+    }
+	else if (!strcmp(values, "C"))
+	{
+        data->map->c_rgb[0] = ft_atoi(values[1]);
+        data->map->c_rgb[1] = ft_atoi(values[2]);
+        data->map->c_rgb[2] = ft_atoi(values[3]);
+    }
+}
+
 void error(char *str)
 {
 	ft_putstr_fd(str, 2);
@@ -45,30 +73,33 @@ static int	count_words(char const *str, char c, char a)
 	return (count);
 }
 
-void parse_side(char *side, int count)
+void parse_side(t_data *data, char *identifier, char *path, int count)
 {
-	if (count != 2)
-		error("bzzf args\n");
-	int len = strlen(side);
-	if (len < 4 || strcmp(side + len - 5, ".xpm\n"))
-		error("no_pic\n");
+    if (count != 2)
+        error("Invalid number of arguments for texture");
+    int len = strlen(path);
+    if (len < 4 || strcmp(path + len - 4, ".xpm"))
+        error("Invalid texture file format");
+    add_data(data, identifier, path);
 }
 
-void parse_fc(char **av, int count)
+void parse_fc(t_data *data, char *identifier, char **values, int count)
 {
-	if (count != 4)
-		error("bzzf args\n");
-	int r = ft_atoi(av[1]);
-	int g = ft_atoi(av[2]);
-	int b = ft_atoi(av[3]);
-	if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
-		error("no_color");
+    if (count != 4)
+        error("Invalid number of arguments for color");
+    int r = ft_atoi(values[1]);
+    int g = ft_atoi(values[2]);
+    int b = ft_atoi(values[3]);
+    if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
+        error("Invalid color values");
+    add_color(data, identifier, r, g, b);
 }
 
 void parsing(t_map *map, int fd)
 {
 	char **work;
 	char *line;
+	t_data	*data;
 	int i =0;
 	int count = 0;
 
@@ -79,18 +110,18 @@ void parsing(t_map *map, int fd)
 		count = count_words(line, 32, ',');
 		work = ft_split(line, count);
 		print_split(work);
-		if(strcmp(work[0], "NO") == 0)
-			parse_side(work[1], count);
-		else if(strcmp(work[0], "SO") == 0)
-			parse_side(work[1], count);
-		else if(strcmp(work[0], "EA") == 0)
-			parse_side(work[1], count);
-		else if(strcmp(work[0], "WE") == 0)
-			parse_side(work[1], count);
-		else if(strcmp(work[0], "F") == 0)
-			parse_fc(work, count);
-		else if(strcmp(work[0], "C") == 0)
-			parse_fc(work, count);
+        if (strcmp(work[0], "NO") == 0)
+            parse_side(&data, work[0], work[1], count);
+        else if (strcmp(work[0], "SO") == 0)
+            parse_side(&data, work[0], work[1], count);
+        else if (strcmp(work[0], "EA") == 0)
+            parse_side(&data, work[0], work[1], count);
+        else if (strcmp(work[0], "WE") == 0)
+            parse_side(&data, work[0], work[1], count);
+        else if (strcmp(work[0], "F") == 0)
+            parse_fc(&data, work[0], work, count);
+        else if (strcmp(work[0], "C") == 0)
+            parse_fc(&data, work[0], work, count);
 		// else if(is_map(line))// to do
 		// 	parsing_map(line , map);// to do
 		// else (isspace(line))
---
+++ Mandatory/parsing/init_data.c
@@ -0,0 +1,2 @@
+#include "../Cub3d.h"
+
